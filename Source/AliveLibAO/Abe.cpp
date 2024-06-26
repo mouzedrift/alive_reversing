6f37dd22fbe5c2f6b5feda20486f2108e2f251aa
mouzedrift <43358824+mouzedrift@users.noreply.github.com>
2024-06-26 16:01:26 +0200

    fix `RunTryEnterWell` not returning true when entering well express

--- Source/AliveLibAO/Abe.cpp
+++ Source/AliveLibAO/Abe.cpp
@@ -1049,6 +1049,7 @@ bool Abe::RunTryEnterWell()
             mSnapAbe = false;
             BaseAliveGameObjectPathTLV = pWellExpress;
             mCurrentMotion = eAbeMotions::Motion_80_430EF0;
+            return true;
         }
     }
 
